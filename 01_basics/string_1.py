0aab9586566b51967c86121b4c6112d12d3a3aaa
akshayrakhunde01@gmail.com <akshayrakhunde01@gmail.com>
2024-12-03 23:46:01 +0530

    added order

--- 01_basics/string_1.py
+++ 01_basics/string_1.py
@@ -2,3 +2,11 @@ chai = 'hii'
 
 # Correct way to call the upper() method
 print(chai.upper())  # This will print 'HII'
+newChai = 'ginger tea, one tea , two tea'
+
+print(newChai.split(", "))
+count =2
+typeOfTea= 'ginger tea'
+anotherExample = ' i ordered {} cup of {} tea'
+
+print(anotherExample.format(count, typeOfTea))
