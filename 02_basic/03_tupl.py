dd601ba5766575c68b6ca3a45f9077d18b5e77cf
akshayrakhunde01@gmail.com <akshayrakhunde01@gmail.com>
2025-01-17 23:16:30 +0530

    basic index and count method

--- 02_basic/03_tupl.py
+++ 02_basic/03_tupl.py
@@ -1,5 +1,9 @@
-tup =(1,2,3,4,5)
-print(type(tup))
-print(tup)
+# tup =(1,2,3,4,5)
+# print(type(tup))
+# print(tup)
 
-print(tup[2])
+# print(tup[2])
+
+tup=(1,5,22,3,4,5,5,5,7,5)
+print(tup.index(5))
+print(tup.count(5))
