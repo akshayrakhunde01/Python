e9dba4cae6a5b671f11ed9f1717798007bec52ce
akshayrakhunde01@gmail.com <akshayrakhunde01@gmail.com>
2025-01-20 23:39:59 +0530

    movies problem

---
+++ Py_with_any/movieInput.py
@@ -0,0 +1,13 @@
+movies=[]
+movie1=input("enter movie 1: ")
+print(movies)
+movie2=input("enter movie 2: ")
+print(movies)
+movie3=input("enter movie 3: ")
+
+movies.append(movie1)
+movies.append(movie2)
+movies.append(movie3)
+
+print(movies)
+
