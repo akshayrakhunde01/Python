faf82ab126b0bfd1a7112698d739c4b2a635c943
akshayrakhunde01@gmail.com <akshayrakhunde01@gmail.com>
2025-01-22 23:58:11 +0530

    method of sets

--- sets/01_set.py
+++ sets/01_set.py
@@ -1,4 +1,13 @@
 
-collection={1,2,3,4,5,6,7}
+# collection={1,2,3,4,5,6,7}
 
-print(type(collection))
+# print(type(collection))
+# set are mutable
+
+colletction =set()
+# set.add(ele) add elemet 
+#  set.remove(ele ) # remove element
+# set.clear # empties the set
+
+# set.pop # remove the elemnt
+ 
